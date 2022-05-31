40e6f48f1379b0ed953685b11a52909aa8875e2d
khisasweet <105873391+khisasweet@users.noreply.github.com>
2022-05-30 22:05:38 -0400

    Create Inheritance_2.py

---
+++ Inheritance_2.py
@@ -0,0 +1,33 @@
+
+class Animal:
+  def __init__(self, type):
+    self.type = type
+
+class Mammal(Animal):
+  def __init__(self, animal):
+    Animal.__init__(self, "mammal")
+    self.animal = animal
+
+  def breathe(self):
+      print("Breathing...")
+
+class Dog(Mammal):
+  def __init__(self, breed):
+    Mammal.__init__(self, "dog")
+    self.breed = breed
+
+  def bark(self):
+      print("Woof")
+      
+  def __str__(self):
+    return self.name + ' is a ' + self.breed
+
+crocodile = Animal("reptile")
+dolphin = Mammal("dolphin")
+pet = Dog("Labrador")
+
+print(dolphin.type)
+print(dolphin.breathe())
+print(pet.type)
+print(pet.breathe())
+print(pet.bark())
