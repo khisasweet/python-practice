e31591dd41b6030e218a0beb3c3aac2d41a2d94f
khisasweet <105873391+khisasweet@users.noreply.github.com>
2022-05-31 21:07:02 -0400

    Create Polymorphism_1.py

---
+++ Polymorphism_1.py
@@ -0,0 +1,26 @@
+### The ability to change the superclass' behavior is called polymorphism #####
+
+class Tracks:
+  def change_direction(self, left, on):
+    print("tracks: ", left, on)
+
+class Wheels:
+  def change_direction(self, left, on):
+    print("wheels: ", left, on)
+
+class Vehicle:
+  def __init__(self, controller):
+    self.controller = controller
+  def turn(self, left):
+    self.controller.change_direction(left, True)
+    timer.sleep(0.25)
+    self.controller.change_direction(left, False)
+
+
+    wheeled = Vehicle(Wheels())
+    tracked = Vehicle(Tracks())
+
+    wheeled.turn(True)
+    tracked.turn(False)
+    
+    
