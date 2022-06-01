2373f725cf1f1d6ed137ad88d00e74d057d7830e
khisasweet <105873391+khisasweet@users.noreply.github.com>
2022-05-31 22:19:27 -0400

    Create exercise_1.py

---
+++ exercise_1.py
@@ -0,0 +1,16 @@
+class Desert:
+    def __init__(self, flavor):
+        self.flavor = flavor
+
+
+class IceCream(Desert):
+    def __init__(self, flavor):
+        super().__init__( flavor)
+
+    # Add a custom string representation
+    def __str__(self):
+        return "My favorite flavor is " + self.flavor
+
+# Create an object IceCream with a pistachio flavor
+obj = IceCream("pistachio")
+print(obj)
