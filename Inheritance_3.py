a0fb71986f6c8402a258d75b1527629500916021
khisasweet <105873391+khisasweet@users.noreply.github.com>
2022-05-31 20:35:07 -0400

    Create Inheritance_3.py

---
+++ Inheritance_3.py
@@ -0,0 +1,38 @@
+
+class Animal:
+  def __init__(self, type):
+    self.type = type
+
+class Mammal(Animal):
+  def __init__(self, animal):
+    super.__init__("mammal")
+    self.animal = animal
+
+  def breathe(self):
+      print("Breathing...")
+
+class Dog(Mammal):
+  def __init__(self, breed):
+    super.__init__("dog")
+    self.breed = breed
+
+  def bark(self):
+      print("Woof")
+      
+  def __str__(self):
+    return self.name + ' is a ' + self.breed
+
+crocodile = Animal("reptile")
+dolphin = Mammal("dolphin")
+pet = Dog("Labrador")
+
+print(dolphin.type)
+print(dolphin.breathe())
+print(pet.type)
+print(pet.breathe())
+print(pet.bark())
+
+print(isinstance(pet,Dog))
+print(isinstance(pet,Animal))
+print(isinstance(dolphin,Dog))
+print(isinstance(dolphin,Animal))
