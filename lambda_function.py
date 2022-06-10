824b4908824d06408004e71d40f6bdac2b3685ff
khisasweet <105873391+khisasweet@users.noreply.github.com>
2022-06-09 21:44:05 -0400

    Create lambda_function.py

---
+++ lambda_function.py
@@ -0,0 +1,7 @@
+### A lambda function is a small anonymous function.
+### A lambda function can take any number of arguments, but can only have one expression.
+### lambda arguments : expression
+
+### Multiply argument a with argument b and return the result:
+x = lambda a, b : a * b
+print(x(5, 6)) 
