07553a00b159d0e94b86098f4382821d242468de
khisasweet <105873391+khisasweet@users.noreply.github.com>
2022-06-09 21:20:21 -0400

    Update Generator_1.py

--- Generator_1.py
+++ Generator_1.py
@@ -1,3 +1,25 @@
 
 ### Generator is a
 ### Specialized code that's able to return a series of values, and to control the iteration process
+
+class CustomRange:
+  def __init__(self, range):
+    self.__range =range
+    self.__i = 0
+    
+  def __iter__(self):
+    return self
+
+  def __next__(self):
+    res = self.__i
+    self.__i += 1
+    print("res: ", str(res))
+    if self.__i > self.__range:
+      raise StopIteration
+    return res 
+    
+r = CustomRange(10)
+
+print(r.__next__())
+print(r.__next__())
+print(r.__next__())
