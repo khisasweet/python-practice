a82760204d0e1283a7da0b4144918d7208de38e6
khisasweet <105873391+khisasweet@users.noreply.github.com>
2022-06-09 21:46:46 -0400

    Create filter_function.py

---
+++ filter_function.py
@@ -0,0 +1,17 @@
+### The filter() function returns an iterator were the items are filtered through a function to test if the item is accepted or not.
+### filter(function, iterable)
+
+### Filter the array, and return a new array with only the values equal to or above 18:
+
+ages = [5, 12, 17, 18, 24, 32]
+
+def myFunc(x):
+  if x < 18:
+    return False
+  else:
+    return True
+
+adults = filter(myFunc, ages)
+
+for x in adults:
+  print(x) 
