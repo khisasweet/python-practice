2b542d8b317beaec4569f74afd4c49ea45582e2f
khisasweet <105873391+khisasweet@users.noreply.github.com>
2022-06-12 21:19:51 -0400

    Update closure.py

--- closure.py
+++ closure.py
@@ -15,3 +15,10 @@ def outerFunction(text):
 if __name__ == '__main__':
     myFunction = outerFunction('Hey!')
     myFunction()
+
+    #
+    # As observed from the above code, closures help to invoke functions outside their scope.
+    # The function innerFunction has its scope only inside the outerFunction. 
+    # But with the use of closures, we can easily extend its scope to invoke a function outside its scope.
+     
+    
