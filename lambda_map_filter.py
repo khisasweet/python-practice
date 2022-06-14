8dda3dca27e7bf89e46f5d5eda146fa81df9f949
khisasweet <105873391+khisasweet@users.noreply.github.com>
2022-06-13 21:44:34 -0400

    Update lambda_map_filter.py

--- lambda_map_filter.py
+++ lambda_map_filter.py
@@ -9,3 +9,31 @@ print(d)
 mix_list = [1, "Python", 0.5, "Apple", 3.0, "\n"]
 numbers = list(filter(lambda x: isinstance(x, float) or isinstance(x, int),  mix_list))
 print(numbers)
+
+
+mix_list = [1, "Python", 0.5, "Apple", 3.0, "\n"]
+numbers = list(filter(
+    lambda n: isinstance(n, float) or isinstance(n, int), mix_list
+    ))
+
+print(numbers)
+
+events = [
+        {'date': 11212021, 'name': 'Bengali wedding'},
+        {'date': 11012021, 'name': 'Project meeting'},
+        {'date': 11112021, 'name': 'Guitar class'},
+]
+
+# Add the sorted code here
+sorted_events = sorted(events, key=lambda x: x['date'])
+print(sorted_events)
+
+# Create the counter generator
+def counter(n):
+    i = 0
+    while i <= n:
+        yield i
+        i += 1
+
+c = counter(3)
+print(next(c), next(c), next(c), next(c), next(c))
